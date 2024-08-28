426a152fc6e6718e0b31004f9fb9ff12cee2eb1b
sriram voonna <sriram.voonna@teladochealth.com>
2024-08-28 11:00:11 -0400

    p6-migrations
    Add new fields to Movies table
    description and released_on and update database with these fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_12_133707) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_28_142611) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
     t.decimal "total_gross"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.date "released_on"
   end
 
 end
